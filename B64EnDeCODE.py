9dd4d69fdb83fc5799416359da6e74bbff3659fc
Darkerego <darkerego@users.noreply.github.com>
2015-12-30 20:01:48 -0500

    Update B64EnDeCODE.py
    Works with or without bad padding now

--- B64EnDeCODE.py
+++ B64EnDeCODE.py
@@ -1,41 +1,58 @@
 #!/usr/bin/env python
-# Uncomplicated Base64 En/De/Coder
-import base64
+# -*- coding: utf-8 -*-
+# Author Darkerego, references:
+# http://stackoverflow.com/questions/2941995/python-ignore-incorrect-padding-error-when-base64-decoding
 """
 ╔╗ ┌─┐┌─┐┌─┐ ╔═╗╔╗╔  ╔╦╗╔═╗╔═╗┌─┐╔╦╗╔═╗
 ╠╩╗├─┤└─┐├┤ 6║╣ ║║║ / ║║║╣ ║  │ │ ║║║╣ 
 ╚═╝┴ ┴└─┘└─┘4╚═╝╝╚╝/ ═╩╝╚═╝╚═╝└─┘═╩╝╚═╝
 """
-def enCODE():
-    string=(input("Paste ASCII to Encode :"))
-                               # Python 3+ will crash without 
-    string=(str.encode(string))#this line
-    enc = base64.b64encode(string).decode('utf-8')
-    print ('\nResult:\n\n',enc,'\n')
 
-def deCODE():
-    dstring=(input("Paste Base64 to Decode: "))
-    # Use this rather than "base64.b64decode(s)"
-    de = base64.urlsafe_b64decode(dstring)
-    print ('\nResult:\n\n',de,'\n')
+import base64,sys,binascii,sys
 
+def b64encode(s, altchars=None):
+    """Encode a string using Base64.
 
+    s is the string to encode.  Optional altchars must be a string of at least
+    length 2 (additional characters are ignored) which specifies an
+    alternative alphabet for the '+' and '/' characters.  This allows an
+    application to e.g. generate url or filesystem safe Base64 strings.
 
-while True:
+    The encoded string is returned.
+    """
+    # Strip off the trailing newline
+    encoded = binascii.b2a_base64(s)[:-1]
+    if altchars is not None:
+        return _translate(encoded, {'+': altchars[0], '/': altchars[1]})
+    return encoded
+
+def urlsafe_b64decode(s):
+    """Encode a string using a url-safe Base64 alphabet.
+       If TypeError 'Incorrect Padding" is raised, fix it.
+    """
+    s = str(s).strip()
     try:
-        doWhat = int(input("Enter 0 to encode or 1 to decode: "))
-    except ValueError:
-        print("Dude...")
-        
-        continue
-    else:
-        if doWhat <= 1:
-            break
+        return base64.b64decode(s)
+    except TypeError:
+        padding = len(s) % 4
+        if padding == 1:
+ 
+            return ''
+        elif padding == 2:
+            s += b'=='
+        elif padding == 3:
+            s += b'='
+        return base64.b64decode(s)
 
-if doWhat == 0:
-    enCODE()
-elif doWhat == 1:
-    deCODE()
-else:
-    print("WTF?")
-    exit(1)
+def main():
+    doWhat = sys.argv[1]
+    s = str(raw_input("Paste stuff to en/de/code :"))
+    if doWhat == "-e":
+        encoded = b64encode(s, altchars=None)
+        print("\n"+"Base64 Encoded Text: "+"\n"+"%s" % encoded)
+    elif doWhat == "-d":
+         decoded = urlsafe_b64decode(s)           
+         print("\n"+"Base64 Decoded Text:"+"\n"+"%s" % decoded)
+
+if __name__ == "__main__":
+    main()
